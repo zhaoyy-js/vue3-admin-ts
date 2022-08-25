a71ed6683b47393678ebf09c1852f8a450b214fd
zhaoyy <16621097346@163.com>
2022-08-25 15:30:09 +0800

    feat: 添加路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,32 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [vue()],
-  // server: {
-  //   port: 3000,
-  //   host: "0.0.0.0",
-  //   proxy: {
-  //     "/": {
-  //       target: "https://fsl.fintify.cn/",
-  //       ws: false,
-  //       changeOrigin: true,
-  //     },
-  //   },
-  // },
+  mode: "development",
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    fs: {
+      strict: true,
+    },
+    proxy: {
+      "/": {
+        target: "https://fsl.fintify.cn/",
+        ws: false,
+        changeOrigin: true,
+      },
+      // "/api": {
+      //   target: "http://jsonplaceholder.typicode.com",
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, ""),
+      // },
+    },
+  },
 });
